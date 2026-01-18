62770cae353322694d748d1ee738a3c1ad475f2c
growpixels <admin@growpixels.in>
2026-01-18 15:26:10 +0530

    logo issue

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -1,20 +1,22 @@
 import Image from 'next/image';
 import { AppConfig } from '@/utils/AppConfig';
 
-export const Logo = (props: { isTextHidden?: boolean }) => (
-  <div className="flex items-center">
-    <Image
-      src="/logo.svg"
-      alt={AppConfig.name}
-      width={180}
-      height={48}
-      priority
-      className="h-12 w-auto"
-    />
-    {!props.isTextHidden && (
-      <span className="ml-2 text-2xl font-semibold">
-        {AppConfig.name}
-      </span>
-    )}
-  </div>
-);
+export const Logo = ({ isTextHidden }: { isTextHidden?: boolean }) => {
+  return (
+    <div className="flex items-center gap-2">
+      <Image
+        src="/logo.svg"
+        alt={AppConfig.name}
+        width={40}
+        height={40}
+        priority
+      />
+
+      {!isTextHidden && (
+        <span className="text-xl font-semibold">
+          {AppConfig.name}
+        </span>
+      )}
+    </div>
+  );
+};
