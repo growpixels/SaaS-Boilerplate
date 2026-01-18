2f4af1352f93c55b5311a968fe70ed450d8a6435
growpixels <admin@growpixels.in>
2026-01-18 15:56:48 +0530

    Refactor Logo component to simplify rendering
    Removed props and updated logo rendering method.

--- src/templates/Logo.tsx
+++ src/templates/Logo.tsx
@@ -1,26 +1,11 @@
-import Image from 'next/image';
-import { AppConfig } from '@/utils/AppConfig';
-
-type LogoProps = {
-  isTextHidden?: boolean;
-};
-
-export const Logo = ({ isTextHidden = false }: LogoProps) => {
+export const Logo = () => {
   return (
-    <div className="flex items-center gap-2">
-      <Image
-        src="/logo.svg"
-        alt={AppConfig.name}
-        width={32}
-        height={32}
-        unoptimized
-      />
-
-      {!isTextHidden && (
-        <span className="text-lg font-semibold">
-          {AppConfig.name}
-        </span>
-      )}
-    </div>
+    <img
+      src="/logo.png"
+      alt="Logo"
+      width={32}
+      height={32}
+      style={{ display: 'block' }}
+    />
   );
 };
